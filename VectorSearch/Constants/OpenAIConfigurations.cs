b94467bd4b471c703109616e332566479cedb2f5
Avishek kunwar <avishekkunwar.work@gmail.com>
2025-07-26 17:11:00 +0545

    Initial Commit

--- VectorSearch/Constants/OpenAIConfigurations.cs
+++ VectorSearch/Constants/OpenAIConfigurations.cs
@@ -3,7 +3,7 @@
 public static class OpenAIConfigurations
 {
     public const string Url = "https://api.openai.com/v1/";
-    public const string Key = "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";
+    public const string Key = "KEY HERE";
     public const string SystemPrompt = "You are an AI assistant that answers user queries using provided context from a semantic search system";
     public const string Model = "gpt-4o-mini";
 }
